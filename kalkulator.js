4cda3140b35ac4a23afca8e630b9a928cec0050f
Hiro <114798397+Hiro24-gif@users.noreply.github.com>
2022-12-05 11:43:50 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -3,7 +3,7 @@ console.log('--------------------');
 console.log('+ = tambah')
 console.log('- = kurang')
 console.log('x = kali')
-console.log('% = bagi')
+console.log('/ = bagi')
 console.log('--------------------');
 let metod = wow.question('pilih metode ')
 let bill2 = wow.questionInt('input bilangan kedua ')
@@ -17,7 +17,7 @@ if (metod == '+') {
         if (metod == 'x') {
             console.log(bill1 * bill2)
         } else {
-            if (metod == '%') {
+            if (metod == '/') {
                 console.log(bill1 / bill2)
             } else {
                 console.log('masukan metode yang benar')
